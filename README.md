f205986d3c490e257d79da26513ac29b4ace49b0
sftn0928 <kobayashi19990928@gmail.com>
2020-06-23 20:57:21 +0900

    設定更新

--- README.md
+++ README.md
@@ -1,22 +1,5 @@
-# test
+# portfolio
+Nuxt.js＋Vuetify.jsで作成した自己紹介サイトです
 
-> My super Nuxt.js project
+リンク　https://sftn0928.netlify.app/
 
-## Build Setup
-
-```bash
-# install dependencies
-$ npm install
-
-# serve with hot reload at localhost:3000
-$ npm run dev
-
-# build for production and launch server
-$ npm run build
-$ npm run start
-
-# generate static project
-$ npm run generate
-```
-
-For detailed explanation on how things work, check out [Nuxt.js docs](https://nuxtjs.org).
--- nuxt.config.js
+++ nuxt.config.js
@@ -6,8 +6,11 @@ export default {
    ** Headers of the page
    */
   head: {
-    titleTemplate: "%s - " + process.env.npm_package_name,
-    title: process.env.npm_package_name || "",
+    titleTemplate: (titleChunk) => {
+      return titleChunk ? titleChunk+" | Yosuke's portfolio" : "Yosuke's portfolio";
+    },
+    // titleTemplate: "%s - " + process.env.npm_package_name,
+    // title: process.env.npm_package_name || "",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
@@ -32,7 +35,6 @@ export default {
    */
   plugins: [
     "~plugins/scrollInview.js"
-    // "~plugins/observe-visibility"
   ],
   /*
    ** Nuxt.js dev-modules
